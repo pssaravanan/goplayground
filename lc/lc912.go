19c30033642ccbc99007e5fd9edd28280413f8c6
Saravanan <saravanan.s@sigmoidanalytics.com>
2024-07-25 22:52:19 +0530

    lc912

---
+++ lc/lc912.go
@@ -0,0 +1,34 @@
+package lc
+
+import "fmt"
+
+func SortArray(nums []int) []int {
+	if len(nums) < 2 {
+		return nums
+	} else if len(nums) == 2 {
+		if nums[0] > nums[1] {
+			return []int{nums[1], nums[0]}
+		} else {
+			return []int{nums[0], nums[1]}
+		}
+	} else {
+		mid := len(nums) / 2
+		leftarray := SortArray(nums[:mid])
+		rightarray := SortArray(nums[mid:])
+		var result []int
+		i := 0
+		j := 0
+		fmt.Println(nums, leftarray, rightarray)
+		for i < len(leftarray) || j < len(rightarray) {
+			if j >= len(rightarray) || leftarray[i] < rightarray[j] {
+				result = append(result, leftarray[i])
+				i = i + 1
+			} else if i >= len(leftarray) || rightarray[j] < leftarray[i] {
+				result = append(result, rightarray[j])
+				j = j + 1
+			}
+		}
+		fmt.Println("index", i, j)
+		return result
+	}
+}
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/pssaravanan/goplayground/lc"
 )
 
@@ -8,5 +10,7 @@ func main() {
 	// result := lc.SortPeople([]string{"Mary", "John", "Emma"}, []int{180, 165, 170})
 	// fmt.Println(result)
 	// lc.ReverseParentheses("(ed(et(oc))el)")
-	lc.SortJumbled([]int{8,9,4,0,2,1,3,5,7,6}, []int{991,338,38})
+	// lc.SortJumbled([]int{8,9,4,0,2,1,3,5,7,6}, []int{991,338,38})
+	result := lc.SortArray([]int{5, 1, 1, 2, 0, 0})
+	fmt.Println(result)
 }
